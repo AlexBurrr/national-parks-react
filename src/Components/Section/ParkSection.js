272238a7578846ee4c0fa8087e485f8ec31e51e4
Alex Berumen <alexandroberumen@gmail.com>
2021-03-16 19:37:22 -0700

    finished collage section

--- src/Components/Section/ParkSection.js
+++ src/Components/Section/ParkSection.js
@@ -1,25 +1,33 @@
 import React from 'react'
 import './Section.scss'
-import HBR from '../../Images/horseback.jpg'
+import HRB from '../../Images/horsebackRiding.jpg'
+import biking from '../../Images/biking.jpg'
+import climbing from '../../Images/climbing.jpg'
+import camping from '../../Images/camping.jpg'
 
 const ParkSection = () => {
     return (
         <div className="sectionContainer">
+            <h1 className='rec'>Recreational Opportunities</h1>
             <div class="grid-container">
-                <div class="wild-life">
-                    <img src={HBR} alt="/" />
+                <div class='camping'>
+                    <img src={camping} alt="/" />
+                    <span className='camping-span'>Camping</span>
 
                 </div>
-                <div class="Hiking">
-                    <img src="" alt="/" />
+                <div class="biking">
+                    <img src={biking} alt="/" />
+                    <span className='bike-span'>Biking</span>
 
                 </div>
                 <div class="picture">
-                    <img src="" alt="/" />
+                    <img src={HRB} alt="/" />
+                    <span className='hrb'>Horse Back Riding</span>
 
                 </div>
-                <div class="picture-2">
-                    <img src="" alt="/" />
+                <div class="climbing">
+                    <img src={climbing} alt="/" />
+                    <span className='climbing-span'>Climbing</span>
 
                 </div>
             </div>
